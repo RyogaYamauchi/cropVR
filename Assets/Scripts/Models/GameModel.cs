9b082c28b3bbece900019fe57a91ec6dc68a7388
Mosunaga <42904550+Mosunaga@users.noreply.github.com>
2019-12-07 17:58:20 +0900

    入力検出

---
+++ Assets/Scripts/Models/GameModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Scripts.Models
+{
+    public class GameModel
+    {
+        public static GameModel Instance { get; } = new GameModel(); //シングルトン
+
+        public int Score;
+    }
+}
---
+++ Assets/Scripts/Views/GameView.cs
@@ -0,0 +1,28 @@
+﻿using Scripts.Models;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameView : MonoBehaviour
+    {
+        private GameModel _gameModel = GameModel.Instance;
+
+        private void Update()
+        {
+            if (OVRInput.GetDown(OVRInput.RawButton.A))
+            {
+                Debug.Log("Aボタンを押した");
+            }
+
+            if (OVRInput.GetDown(OVRInput.RawButton.B))
+            {
+                Debug.Log("Bボタンを押した");
+            }
+
+            if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+            {
+                Debug.Log("右人差し指トリガーを押した");
+            }
+        }
+    }
+}
